84df110b86c7813d2e69a215e90ee639f5d81d35
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-04-28 10:40:30 +0200

    final changes

--- rest/dao/MidtermDao.php
+++ rest/dao/MidtermDao.php
@@ -55,7 +55,8 @@ class MidtermDao {
 
       $stmt = $this->conn->prepare($query);
       $stmt->execute();
-      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+      //return $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     /** TODO
